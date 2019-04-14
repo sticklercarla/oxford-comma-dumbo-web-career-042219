7f5173cc09f38ddefa887423530573a8ca552c4d
Carla Stickler <carlastickler@gmail.com>
2019-04-14 23:34:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length ==1 
+  if array.length == 1 
     array.join(" , ")
-  elsif array.length == 3
+  elsif array.length == 2
     array.join(" and ")
   else 
     last_item = array.pop
