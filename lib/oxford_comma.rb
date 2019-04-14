bf8eeb0873ecf2c5f8a0d5536d80c12cd2c8777a
Carla Stickler <carlastickler@gmail.com>
2019-04-14 23:34:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join(" , ")
+  elsif array.length == 2
+    array.join(" and ")
+  else 
+    last_item = array.pop
+    new_array = array.join(", ") + ", and " + last_item
+    new_array
+  end
 end
